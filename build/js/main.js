2cad8a49317c58873d3b1125c1396614e846b156
NickBarkerDev <nick.barker.devops@gmail.com>
2023-09-22 23:15:23 -0700

    added background color to save eyes

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,6 @@
 "use strict";
 let user = "Nick";
 console.log(user);
+let a = 42;
+let b = '69';
+let c = 34;
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
 let user = "Nick";
 
 console.log(user);
+
+let a = 42
+let b = '69'
+let c = 34
