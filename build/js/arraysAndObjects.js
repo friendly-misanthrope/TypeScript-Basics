b90cc0d72b0f6b6e4ca8f0c319ebcfa3aeb55e48
NickBarkerDev <nick.barker.devops@gmail.com>
2023-09-23 21:49:54 -0700

    Arrays, objects, and Enums complete

--- build/js/arraysAndObjects.js
+++ build/js/arraysAndObjects.js
@@ -14,3 +14,42 @@ mixed = myTuple;
 // illegal
 //! myTuple = mixed
 // Objects
+// legal, because an array is an object in JS
+let myObj;
+myObj = [];
+let exampleObj = {
+    prop1: "Nick",
+    prop2: true
+};
+// Creating an instance of custom type
+let eddieVh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, 'OU812']
+};
+let jimmyP = {
+    name: "Jimmy",
+    active: true,
+    albums: ['I', 'II', 'IV']
+};
+// This is valid because they both have the same 3 properties and data types for each property.
+eddieVh = jimmyP;
+// Optional properties
+//! line 39 - Question mark after property denotes that it can either be boolean or undefined (optional)
+const greetGuitarist = (guitarist) => {
+    if (guitarist) {
+        return `Hello ${guitarist.name}!`;
+    }
+    return 'Hello!';
+};
+console.log(greetGuitarist(jimmyP));
+// Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- src/arraysAndObjects.ts
+++ src/arraysAndObjects.ts
@@ -1,7 +1,7 @@
 // Arrays
-let stringArr: string[] = ['one', 'hey', 'Nick']
-let guitars: (string | number)[] = ["strat", "les paul", 5150]
-let mixedData:(string | number | boolean)[] = ['EVH', 1984, true]
+    let stringArr: string[] = ['one', 'hey', 'Nick']
+    let guitars: (string | number)[] = ["strat", "les paul", 5150]
+    let mixedData:(string | number | boolean)[] = ['EVH', 1984, true]
 
     //! Nope
     // stringArr[3] = 42
@@ -16,4 +16,67 @@ let mixedData:(string | number | boolean)[] = ['EVH', 1984, true]
     // illegal
     //! myTuple = mixed
 
+
+
 // Objects
+    // legal, because an array is an object in JS
+    let myObj: object
+    myObj = []
+
+    let exampleObj = {
+        prop1: "Nick",
+        prop2: true
+    }
+
+    // cannot reassign obj props after initial type declaration. Once declared, it's locked in to initial type.
+    //! illegal
+    // exampleObj = {
+    //     prop1: "Nicholas",
+    //     prop2: 42
+    // }
+
+    // Defining custom type
+    type Guitarist = {
+        name: string,
+        active?: boolean,
+        albums: (string | number)[],
+    }
+
+    // Creating an instance of custom type
+    let eddieVh: Guitarist = {
+        name: "Eddie",
+        active: false,
+        albums: [1984, 5150, 'OU812']
+    }
+
+    let jimmyP: Guitarist = {
+        name: "Jimmy",
+        active: true,
+        albums: ['I', 'II', 'IV']
+    }
+    // This is valid because they both have the same 3 properties and data types for each property.
+    eddieVh = jimmyP
+
+    // Optional properties
+    //! line 39 - Question mark after property denotes that it can either be boolean or undefined (optional)
+
+    const greetGuitarist = (guitarist: Guitarist) => {
+        if (guitarist) {
+            return `Hello ${guitarist.name}!`
+        }
+        return 'Hello!'
+    }
+    console.log(greetGuitarist(jimmyP))
+
+
+
+    // Enums
+
+    enum Grade {
+        U = 1,
+        D,
+        C,
+        B,
+        A
+    }
+    console.log(Grade.U)
